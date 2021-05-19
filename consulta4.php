8a197554686824cb9e45e16b77251b9e23ef82e8
RebecaB18 <61757546+RebecaB18@users.noreply.github.com>
2021-05-19 18:36:55 -0300

    Add files via upload

---
+++ consulta4.php
@@ -0,0 +1,37 @@
+<?php
+    include 'banco.php';
+	$link = bancoConecta();
+
+	$sql = <<<EOT
+
+        SELECT 
+            c.Name, c.Population, c.GNP, c.LifeExpectancy
+        FROM 
+            country as c
+        WHERE
+            c.Continent = 'Africa'
+        ORDER BY 
+            c.GNP ASC
+EOT;
+?>
+<h1>Países africanos</h1>
+<table class="table table-dark">
+	<thead>
+		<tr>
+		<th scope="col">Name</th>
+		<th scope="col">Population</th>
+		<th scope="col">GNP</th>
+		<th scope="col">LifeExpectancy</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach(executaSelect($link,$sql) as $linha) { ?> 
+			<tr>
+				<td><?=$linha['Name']?></td>
+				<td><?=$linha['Population']?></td>
+				<td><?=$linha['GNP']?></td>
+				<td><?=$linha['LifeExpectancy']?></td>
+			</tr>
+		<?php } /*foreach*/ ?>
+	</tbody>
+</table>

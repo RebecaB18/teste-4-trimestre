60d23d01266ac2c34c7e93cb868309cf401ac1ef
RebecaB18 <61757546+RebecaB18@users.noreply.github.com>
2021-05-19 18:37:30 -0300

    Add files via upload

---
+++ consulta2.php
@@ -0,0 +1,31 @@
+<?php
+    include 'banco.php';
+	$link = bancoConecta();
+
+	$sql = <<<EOT
+
+        SELECT 
+            ci.Name, ci.Population
+        FROM 
+            city as ci
+        WHERE
+            ci.CountryCode ='BRA'
+EOT;
+?>
+<h1>Cidades brasileiras e suas populações</h1>
+<table class="table table-dark">
+	<thead>
+		<tr>
+		<th scope="col">Nome</th>
+		<th scope="col">População</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach(executaSelect($link,$sql) as $linha) { ?> 
+			<tr>
+				<td><?=$linha['Name']?></td>
+				<td><?=$linha['Population']?></td>
+			</tr>
+		<?php } /*foreach*/ ?>
+	</tbody>
+</table>

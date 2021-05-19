74714737abe3298254004e22fffd7aeb86364b9a
RebecaB18 <61757546+RebecaB18@users.noreply.github.com>
2021-05-19 18:35:44 -0300

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,10 @@
+<?php
+    include 'banco.php';
+    ?>
+
+ <p><a  href="consulta1.php">Consulta 1</a></p>
+ <p><a  href="consulta2.php">Consulta 2</a></p>
+ <p><a  href="consulta3.php">Consulta 3</a></p>
+ <p><a  href="consulta4.php">Consulta 4</a></p>
+ <p><a  href="consulta5.php">Consulta 5</a></p>
+ <p><a  href="consulta6.php">Consulta 6</a></p>
